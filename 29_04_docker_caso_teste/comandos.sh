36665500c372a5eba909d4aa9f83f66f28e74795
Lucas Purkota <lucaspurkota@gmail.com>
2025-04-29 21:29:54 -0300

    atividades dia 29-04

---
+++ 29_04_docker_caso_teste/comandos.sh
@@ -0,0 +1,9 @@
+docker network create caso-testee
+
+docker build -t test-shell --no-cache .
+
+docker build -t test-shell --no-cache .
+
+docker run --network app -d --name app -p 3000:3000 app
+
+docker run --network app -d --name test-shell test-shell
