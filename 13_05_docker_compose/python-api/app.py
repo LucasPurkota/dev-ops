b3d0a4e5fb4ead73bbccc31ae20bf9c9f302b97a
Lucas Purkota <lucaspurkota@gmail.com>
2025-05-13 21:09:39 -0300

    subindo atividade aula06 dockercompose

---
+++ 13_05_docker_compose/python-api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from dotenv import load_dotenv
+import mysql.connector
+import os
+
+load_dotenv()
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  db = mysql.connector.connect(
+    user=os.environ.get("DB_USER"),
+    host=os.environ.get("DB_HOST"),
+    password=os.environ.get("DB_PASSWORD"),
+    database=os.environ.get("DB_DATABASE"),
+  )
+  
+  cursor = db.cursor()
+  cursor.execute("SELECT 'Hello from Flask and MySQL!'")
+  result = cursor.fetchone()
+  cursor.close()
+  db.close()
+  return result[0]
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
