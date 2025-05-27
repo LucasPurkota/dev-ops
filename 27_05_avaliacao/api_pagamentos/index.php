3bb8ad1e638802c14f2f230a28bee7407e3f6ebf
Lucas Purkota <lucaspurkota@gmail.com>
2025-05-27 20:40:13 -0300

    subindo arquivos da prova de devops

---
+++ 27_05_avaliacao/api_pagamentos/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$request_uri = $_SERVER['REQUEST_URI'];
+
+if ($request_uri === '/payment') {
+    $orderJson = file_get_contents('http://orders:3002/order');
+    $orderData = json_decode($orderJson, true);
+
+    $response = [
+        'status' => 'paid',
+        'order' => $orderData
+    ];
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+} else {
+    http_response_code(404);
+    echo json_encode(['error' => 'Not found']);
+}
+
+// O servidor web pode ser configurado no Dockerfile com:
+// php -S 0.0.0.0:3003
